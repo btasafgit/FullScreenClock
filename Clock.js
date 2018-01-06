51976e190940d7280b210212b674447c32853930
btasaf <btasaf@BTASAF-PC>
2018-01-06 11:01:46 +0200

    New Project: Fullscreen Image with Clock and timeUpdate function (Not working yet)

---
+++ Clock.js
@@ -0,0 +1,12 @@
+﻿
+
+function timeUpdate() {
+    var fullDate = new Date();
+    var date = document.getElementsByClassName("date");
+    var time = document.getElementsByClassName("time");
+    var newDate = fullDate.toLocaleDateString();
+    var newTime = fullDate.toLocaleTimeString();
+
+    date[0].textContent = newDate;
+    time[0].textContent = newTime;
+}
